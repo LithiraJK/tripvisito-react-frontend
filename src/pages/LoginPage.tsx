2215a3d0eee37edb79454439f2765764def8d2b5
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2025-11-24 11:59:34 +0530

    implement login functionality and improve UI

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import React, { useState } from "react";
+import { login } from "../services/auth";
+import { useAuth } from "../contexts/authContext";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {setUser} = useAuth()
+  const navigate = useNavigate()
+
+  const handleLogin = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!email.trim() || !password.trim()) {
+      alert("Please Enter both email and password !");
+      return;
+    }
+
+    try {
+      const response: any = await login(email, password);
+
+      if (response?.data?.accessToken) {
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+
+        const userDetails = response.data.user;
+        setUser(userDetails);
+
+        navigate("/dashboard");
+      }else{
+        alert("Login failed, please check your credentials");
+      }
+
+
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("Login failed, please check your credentials");
+    }
+  };
+
   return (
     <main className='w-full h-screen flex justify-center items-center min-h-screen  bg-[url("src/assets/images/auth-img.webp")] bg-cover bg-no-repeat'>
       <section className="size-full bg-[rgba(255,255,255,0.3)] glassmorphism flex justify-center items-center px-2 sm:px-4 overflow-y-auto">
@@ -25,17 +64,30 @@ const LoginPage = () => {
                 Start Your Travel Journey
               </h2>
               <p className="text-base md:text-lg text-gray-600 leading-relaxed max-w-md">
-                Log in to manage your trips, discover new destinations, and create unforgettable travel experiences.
+                Log in to manage your trips, discover new destinations, and
+                create unforgettable travel experiences.
               </p>
               <div className=" hidden md:flex items-start space-x-3 pt-4">
                 <div className="shrink-0 w-10 h-10 rounded-full bg-linear-to-r from-cyan-400 to-sky-500 flex items-center justify-center">
-                  <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                  <svg
+                    className="w-5 h-5 text-white"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 13l4 4L19 7"
+                    />
                   </svg>
                 </div>
                 <div>
                   <h3 className="font-semibold text-gray-800">Secure & Safe</h3>
-                  <p className="text-sm text-gray-600">Your data is protected with enterprise-grade security</p>
+                  <p className="text-sm text-gray-600">
+                    Your data is protected with enterprise-grade security
+                  </p>
                 </div>
               </div>
             </article>
@@ -47,8 +99,12 @@ const LoginPage = () => {
                 <div className="relative px-4 py-6 xs:px-6 xs:py-8 sm:p-10 bg-white shadow-lg rounded-2xl sm:rounded-3xl">
                   <div className="w-full mx-auto">
                     <div className="flex flex-col justify-center items-center mb-4 sm:mb-6">
-                      <h1 className="sm:text-xl text-2xl md:text-4xl font-bold text-gray-800 mb-2">Login</h1>
-                      <p className="text-lg sm:text-base text-gray-500">Welcome back !</p>
+                      <h1 className="sm:text-xl text-2xl md:text-4xl font-bold text-gray-800 mb-2">
+                        Login
+                      </h1>
+                      <p className="text-lg sm:text-base text-gray-500">
+                        Welcome back !
+                      </p>
                     </div>
                     <div>
                       <div className="space-y-3 sm:space-y-4">
@@ -65,8 +121,8 @@ const LoginPage = () => {
                             type="text"
                             className="text-xs xs:text-sm py-2 px-3 xs:px-4 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-cyan-400"
                             placeholder="Email address"
+                            onChange={(e)=> setEmail(e.target.value)}
                           />
-                          
                         </div>
                         <div className="relative">
                           <label
@@ -80,21 +136,25 @@ const LoginPage = () => {
                             type="password"
                             className="text-xs xs:text-sm py-2 px-3 xs:px-4 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-cyan-400"
                             placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
                           />
-                          
                         </div>
                         <div className="relative pt-1 sm:pt-2">
-                          <button className="w-full bg-linear-to-r from-cyan-400 to-sky-500 text-white text-sm sm:text-base font-semibold rounded-lg px-4 py-2 hover:bg-linear-to-r hover:from-sky-500 hover:to-cyan-400 transition-all hover:cursor-pointer">
+                          <button onClick={handleLogin} className="w-full bg-linear-to-r from-cyan-400 to-sky-500 text-white text-sm sm:text-base font-semibold rounded-lg px-4 py-2 hover:bg-linear-to-r hover:from-sky-500 hover:to-cyan-400 transition-all hover:cursor-pointer">
                             Login
                           </button>
                         </div>
-                        <p className="text-center text-xs sm:text-sm text-gray-500 py-1 sm:py-2"> or </p>
+                        <p className="text-center text-xs sm:text-sm text-gray-500 py-1 sm:py-2">
+                          {" "}
+                          or{" "}
+                        </p>
                         <div className="relative">
-                          <button className="w-full border border-gray-300 rounded-xl py-2 flex items-center justify-center hover:bg-gray-50 transition-all hover:cursor-pointer" >
+                          <button className="w-full border border-gray-300 rounded-xl py-2 flex items-center justify-center hover:bg-gray-50 transition-all hover:cursor-pointer">
                             <FcGoogle className="mr-2 text-lg sm:text-xl" />
-                            <span className="text-xs xs:text-sm sm:text-base">Continue with Google</span>
+                            <span className="text-xs xs:text-sm sm:text-base">
+                              Continue with Google
+                            </span>
                           </button>
-
                         </div>
                       </div>
                     </div>
