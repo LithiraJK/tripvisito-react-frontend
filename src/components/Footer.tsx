5364289b632e82adc3bdea7bf4f7c646064112d2
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-01-09 18:51:17 +0530

    feat: Update Footer background color and adjust LandingLayout minimum width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ const Footer = () => {
 
   
   return (
-    <footer className="bg-linear-to-t from-blue-200 to-sky-50 text-[#112D4E] ">
+    <footer className="bg-white text-[#112D4E] ">
+      
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Brand Section */}
--- src/components/LandingLayout.tsx
+++ src/components/LandingLayout.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar'
 const LandingLayout = () => {
 
   return (
-    <div className='min-h-screen min-w-[375px]'>
+    <div className='min-h-screen min-w-[320px]'>
       <header className='absolute top-0 left-0 w-full glassmorphism border-0 rounded-2xl shadow-lg'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
           <NavBar />
